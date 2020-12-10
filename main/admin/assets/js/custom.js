33d3a892055dcc67515c91540a0969706781e01c
chukkydave <chukkydave@gmail.com>
2020-12-10 15:19:10 +0100

    renamed the index and login pages

--- main/admin/assets/js/custom.js
+++ main/admin/assets/js/custom.js
@@ -5,4 +5,31 @@
  *
  */
 
-"use strict";
+// "use strict";
+
+$(document).ready(function() {
+	checkLoggedin();
+});
+
+('use strict');
+var apiPaths = 'https://love-sense.herokuapp.com/api/v1/';
+var myHome = 'https://www.lovesense.online/admin/index.html';
+
+// love-sense
+
+let namey = localStorage.getItem('user_name');
+let emaily = localStorage.getItem('user_email');
+$('#namey1').html(namey);
+$('#user-name1').html(namey);
+
+$('#logMeOut').click(function() {
+	localStorage.clear();
+	window.location.href = myHome;
+});
+
+function checkLoggedin() {
+	if (localStorage.getItem('token') === null) {
+		var url = myHome;
+		window.location.replace(url);
+	}
+}
