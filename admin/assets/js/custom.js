828d5a42b9e25d6559029d1b37b2f873fd42ffea
Chuks <chukkydave@gmail.com>
2020-12-10 11:06:32 +0100

    first commit

---
+++ admin/assets/js/custom.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * You can write your JS code here, DO NOT touch the default style file
+ * because it will make it harder for you to update.
+ *
+ */
+$(document).ready(function() {
+	checkLoggedin();
+});
+
+('use strict');
+var apiPaths = 'https://love-sense.herokuapp.com/api/v1/';
+var myHome = 'https://www.lovesense.online/admin/index.html';
+
+// love-sense
+
+let namey = localStorage.getItem('user_name');
+let emaily = localStorage.getItem('user_email');
+$('#namey1').html(namey);
+$('#user-name1').html(namey);
+
+$('#logMeOut').click(function() {
+	localStorage.clear();
+	window.location.href = myHome;
+});
+
+function checkLoggedin() {
+	if (localStorage.getItem('token') === null) {
+		var url = myHome;
+		window.location.replace(url);
+	}
+}
