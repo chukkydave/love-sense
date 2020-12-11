1adadd0b2cef6a4b7dfeaa68f6de452de76cc131
chukkydave <chukkydave@gmail.com>
2020-12-11 07:58:48 +0100

    plugged in the profile endpoints but to be revisited

---
+++ main/super-admin/custom_js/profile.js
@@ -0,0 +1,84 @@
+$('#changeBtn').on('click', function() {
+	if (isEmptyInput('.required_f')) {
+		updateInfo();
+	}
+});
+
+let namer = localStorage.getItem('user_name');
+let emailer = localStorage.getItem('user_email');
+let phoner = localStorage.getItem('user_phone');
+$('#myName').html(namer);
+$('#myEmail').html(emailer);
+$('#myPhone').html(phoner);
+
+function isEmptyInput(first) {
+	let isEmpty = false;
+	$(first).each(function() {
+		var input = $.trim($(this).val());
+		if (input.length === 0 || input === '0') {
+			$(this).addClass('has-error');
+			isEmpty = true;
+		} else {
+			$(this).removeClass('has-error');
+		}
+	});
+	if (isEmpty === true) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function updateInfo() {
+	$('#changeBtn').hide();
+	$('#changeLoader').show();
+	//declaring variable using the ES6 let
+	let changeName = $('#cName').val();
+	let changeEmail = $('#cEmail').val();
+	let changePhone = $('#cPhone').val();
+
+	// using the ES6 const variable to hold the url to the api path
+	const URL = `${apiPaths}admin/update_admin`;
+	//storing the variable in a data variable to be an object as Displayed in the PAYLOAD
+
+	let data = {
+		name: changeName,
+		email: changeEmail,
+		phone_number: changePhone,
+	};
+	console.log(data);
+
+	var token1 = localStorage.getItem('token');
+
+	//using AJAX to make the submit call
+	$.ajax({
+		type: 'PATCH',
+		dataType: 'json',
+		cache: false,
+		url: URL,
+		headers: {
+			// 'Accept': 'application/json',
+			'Content-Type': 'application/json',
+			Authorization: token1,
+		},
+		data: JSON.stringify(data),
+		success: (res) => {
+			// alert(res.status)
+			if (res.status == 201 || res.status == 200) {
+				alert('Succesful');
+				$('#changeLoader').hide();
+				$('#changeBtn').show();
+				window.location.reload;
+			} else {
+				alert('Error!!');
+				$('#changeLoader').hide();
+				$('#changeBtn').show();
+			}
+		},
+		error: (res) => {
+			alert(res.responseText);
+			$('#changeLoader').hide();
+			$('#changeBtn').show();
+		},
+	});
+}
