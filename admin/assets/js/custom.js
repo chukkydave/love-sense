bbcfc20e69f30bd393ddf7de90097243cc44fc57
chukkydave <chukkydave@gmail.com>
2021-05-17 11:45:11 +0100

    after notification fix

--- admin/assets/js/custom.js
+++ admin/assets/js/custom.js
@@ -4,8 +4,22 @@
  * because it will make it harder for you to update.
  *
  */
+let counter = 0;
 $(document).ready(function() {
 	checkLoggedin();
+	getNotification();
+	// askNotificationPermission();
+	const notificationBtns = document.getElementById('breathing-button');
+
+	// Do an initial check to see what the notification permission state is
+
+	if (Notification.permission === 'denied' || Notification.permission === 'default') {
+		notificationBtns.style.display = 'block';
+	} else {
+		notificationBtns.style.display = 'none';
+	}
+
+	$('#breathing-button').on('click', askNotificationPermission);
 });
 
 ('use strict');
@@ -28,5 +42,85 @@ function checkLoggedin() {
 	if (localStorage.getItem('token') === null) {
 		var url = myHome;
 		window.location.replace(url);
+	}
+}
+
+function getNotification() {
+	let token1 = localStorage.getItem('token');
+
+	axios
+		.get(`${apiPaths}notifications`, {
+			params: {
+				// ID: 12345
+			},
+			headers: {
+				Authorization: token1,
+			},
+		})
+		.then(function(response) {
+			console.log(response.data);
+			let notLength = response.data.length;
+
+			if (notLength > 0) {
+				counter = notLength;
+				$('#notification_count').html(counter);
+				$('#notification_count').show();
+				$('#notification_icon').addClass('mailAnim');
+				let body = '';
+				$(response.data).map((i, v) => {
+					// let dates = v.date.moment().format("MMM Do YY")
+					let dates = moment(v.date).format('ll');
+					body += `<a href="appointments" class="dropdown-item">  <span class="dropdown-item-desc"> <span class="message-user">${v.first_name} ${v.last_name}
+                    </span> <span class="time messege-text">${v.service}<br> ${v.time} ${dates}</span>
+                  </span>
+                </a>`;
+				});
+				$('#notification_body').html(body);
+			}
+		})
+		.catch(function(error) {
+			console.log(error);
+		})
+		.then(function() {
+			// always executed
+		});
+}
+
+function checkNotificationPromise() {
+	try {
+		Notification.requestPermission().then();
+	} catch (e) {
+		return false;
+	}
+
+	return true;
+}
+
+function askNotificationPermission() {
+	// function to actually ask the permissions
+	const notificationBtn = document.getElementById('breathing-button');
+
+	function handlePermission(permission) {
+		// set the button to shown or hidden, depending on what the user answers
+		if (Notification.permission === 'denied' || Notification.permission === 'default') {
+			notificationBtn.style.display = 'block';
+		} else {
+			notificationBtn.style.display = 'none';
+		}
+	}
+
+	// Let's check if the browser supports notifications
+	if (!('Notification' in window)) {
+		console.log('This browser does not support notifications.');
+	} else {
+		if (checkNotificationPromise()) {
+			Notification.requestPermission().then((permission) => {
+				handlePermission(permission);
+			});
+		} else {
+			Notification.requestPermission(function(permission) {
+				handlePermission(permission);
+			});
+		}
 	}
 }
--- general.js
+++ general.js
@@ -2,9 +2,37 @@ $(document).ready(() => {
 	// alert('welcome');
 	var socket = io.connect('https://love-sense.herokuapp.com/', { forceNew: true });
 
+	// var socket = io.connect('http://localhost:8000/', { forceNew: true });
+
 	socket.on('successful_booking', function(data) {
 		console.log({ data });
-		alert(data);
+		// alert(data);
+		counter = counter + 1;
+		$('#notification_count').html(counter);
+		let body = '';
+
+		let dates = moment(data.resBookings.date).format('ll');
+		body += `<a href="appointments" class="dropdown-item">  <span class="dropdown-item-desc"> <span class="message-user">${data
+			.resBookings.first_name} ${data.resBookings.last_name}
+                    </span> <span class="time messege-text">${data.resBookings.service}<br> ${data
+			.resBookings.time} ${dates}</span>
+                  </span>
+                </a>`;
+		$('#notification_body').append(body);
+		if (window.location.pathname.split('/')[1] == 'admin') {
+			function play2() {
+				/* Audio link for notification */
+				var audio = new Audio('https://lovesense.online/audio/swiftly.mp3');
+				audio.play();
+			}
+			// var audio = new Audio('https://lovesense.online/audio/swiftly.mp3');
+			var img = 'https://lovesense.online/images/logos/logo3.png';
+			var text = `Hey!! ${data.resBookings.first_name} ${data.resBookings
+				.last_name} just booked a ${data.resBookings.service} service scheduled for ${data
+				.resBookings.time} ${dates}`;
+			var notification = new Notification('New Booking', { body: text, icon: img });
+			play2();
+		}
 	});
 });
 
@@ -31,8 +59,9 @@ function makePayment() {
 	}
 
 	FlutterwaveCheckout({
-		// public_key: "FLWPUBK-98cb379fa4bab657de583bf5c16c7a91-X",
-		public_key: 'FLWPUBK_TEST-4a90330b534132a4d82f4389620fdb56-X',
+		public_key: 'FLWPUBK-98cb379fa4bab657de583bf5c16c7a91-X',
+
+		// public_key: 'FLWPUBK_TEST-4a90330b534132a4d82f4389620fdb56-X',
 		tx_ref: '' + Math.floor(Math.random() * 1000000000 + 1),
 		amount: finalAmt,
 		currency: finalCur,
@@ -53,30 +82,44 @@ function makePayment() {
 			// alert(data);
 			// return;
 			var socket = io.connect('https://love-sense.herokuapp.com/', { forceNew: true });
+			// var socket = io.connect('http://localhost:8000/', { forceNew: true });
 
 			console.log(data);
 
 			// console.log(data);
 			var _id = localStorage.getItem('_id');
+			let resBookings = JSON.parse(localStorage.getItem('resBookings'));
 			console.log({ _id });
 			socket.emit('successful_booking', {
 				_id: _id,
+				resBookings: resBookings,
 			});
+			$('#make_payment').hide();
+			localStorage.removeItem('name');
+			localStorage.removeItem('last_name');
+			localStorage.removeItem('email');
+			localStorage.removeItem('service');
+			localStorage.removeItem('price');
+			localStorage.removeItem('phones');
+			localStorage.removeItem('resBookings');
+			setTimeout(() => {
+				window.location.href = 'https://www.lovesense.online';
+			}, 2000);
 		},
 		onclose: function() {
 			// close modal
 		},
 		// success: function(data) {
-		// 	alert(data);
-		// 	return;
-		// 	// var _id = localStorage.getItem('_id');
-		// 	// console.log({ _id });
-		// 	// socket.emit('successful_booking', {
-		// 	// 	_id: _id,
-		// 	// });
-		// 	// setTimeout(() => {
-		// 	// 	localStorage.removeItem(_id);
-		// 	// }, 1000);
+		//  alert(data);
+		//  return;
+		//  // var _id = localStorage.getItem('_id');
+		//  // console.log({ _id });
+		//  // socket.emit('successful_booking', {
+		//  //  _id: _id,
+		//  // });
+		//  // setTimeout(() => {
+		//  //  localStorage.removeItem(_id);
+		//  // }, 1000);
 		// },
 		customizations: {
 			title: 'Love Sense',
